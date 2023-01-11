c8b71dc6807d0f2cc25143cfec31f24c58a32080
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2023-01-11 16:22:26 -0500

    Landing

---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,64 @@
+import "./landing.scss";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+        <div className="top">
+            <div className="left">
+                <h1>
+                    Launch into your professional career today.
+                </h1>
+            </div>
+            <div className="right">
+                <div className="loginbox">
+                    <h2>Sign up for a free account</h2>    
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <button>Login</button>
+                    </form>  
+                </div>  
+            </div>
+        </div>
+        <div className="middle">
+            <div className="left">
+                <h1>Stand out from the crowd with videos.</h1>
+            </div>
+            <div className="right">
+                <img src = ""></img>
+            </div>
+            
+        </div>
+        <div class="bottom">
+            <h1>We humanize the professional job world.</h1>
+            <table>
+                <tr>
+                    <th>Show what your resume doesn't.</th>
+                    <th>Bypass steps in your interview process.</th>
+                </tr>
+                <tr>
+                    <td>Your resume doesn’t show who you are as a person, your enthusiasm, your 
+                        expressions, and so on. We believe that you are more than just a list of job 
+                        experiences and that employers deserve to know that just as much as you 
+                        deserve to show it.</td>
+                    <td>The interview process today is nothing short of a 5+ step process. Tadpole 
+                        allows users to help potentially shorten that interview process by giving 
+                        employers a more in-depth video view of who you are. </td>
+                </tr>
+                <tr>
+                    <th>Connect with peers.</th>
+                    <th>Leap Forward.</th>
+                </tr>
+                <tr>
+                    <td>Tadpole allows you to network with business peers 
+                        and build your community in a more versatile way 
+                        geared for today’s world. </td>
+                    <td>Take the next step in your professional career. Transform. Leap Forward.</td>
+                </tr>
+            </table>
+        </div>  
+    </div>
+  );
+};
+
+export default Landing;
